02ae52b48745aa9fe85cb5516e90538ce2dfe1ed
minoong <alsdndjdrktl@gmail.com>
2020-10-15 21:12:11 +0900

    first commit

---
+++ routes/middlewares.js
@@ -0,0 +1,18 @@
+const { noExtendLeft } = require("sequelize/types/lib/operators");
+
+exports.isLoggedIn = (req, res, text) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(403).send("로그인 필요");
+  }
+};
+
+exports.isNotLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    next();
+  } else {
+    const message = encodeURIComponent("로그인한 상태입니다.");
+    res.redirect(`/?error=${message}`);
+  }
+};
